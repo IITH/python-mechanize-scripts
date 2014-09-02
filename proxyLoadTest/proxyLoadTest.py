cacbaba7d2c4bf5bf780bc14457529257dbbe6b8
G8XSU <g8x000@gmail.com>
2014-09-03 01:55:55 +0530

    removed cookielib

--- proxyLoadTest/proxyLoadTest.py
+++ proxyLoadTest/proxyLoadTest.py
@@ -1,13 +1,10 @@
 import mechanize
-import cookielib
 import time
 from urllib2 import HTTPError
 import Queue
 import threading
 
 br = mechanize.Browser()
-cj= cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
 
 br.set_handle_equiv(True)
 br.set_handle_gzip(True)
@@ -15,7 +12,7 @@ br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
 
-start_time=time.time()
+
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 #br.set_proxies({"http": "joe:password@myproxy.example.com:3128"})
@@ -34,7 +31,7 @@ def open_url(q,url):
 with open('websites.txt') as f:
 	content = f.readlines()
 	
-
+start_time=time.time()
 for url in content:
   t = threading.Thread(target=open_url, args = (q,url))
   t.daemon=True
